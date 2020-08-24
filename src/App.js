ef99fe36c1bac97bd02d117748033ff88d14cc70
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-24 08:13:25 -0700

    Dynamic Array using prop

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Nayoks = ['Jasim','Manna','Maroof','Bapparaz','Razzak']
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Nayok name = "Jasim"></Nayok>
-        <Nayok name = "Shakib Khan"></Nayok>
-        <Nayok name = "Salman Khan"></Nayok>
-        <Nayok name = "Sahrukh Khan"></Nayok>
-        <Nayok></Nayok>
-        <Nayok></Nayok>
-        <Nayok></Nayok>
+        
+        <Nayok name = {Nayoks[0]}></Nayok>
+        <Nayok name = {Nayoks[1]}></Nayok>
+        <Nayok name = {Nayoks[2]}></Nayok>
+        <Nayok name = {Nayoks[3]}></Nayok>
+        <Nayok name = {Nayoks[4]}></Nayok>
+        <Nayok name = {Nayoks[5]}></Nayok>
+        <Nayok name = {Nayoks[6]}></Nayok>
       </header>
     </div>
   );
@@ -21,9 +23,10 @@ function App() {
 
 
 function Nayok(props){
+  
   return ( 
   <div style = {{border:'2px magenta solid', margin:'3px', padding:'5px', borderRadius:'10%', width:'75%', height:'200px'}}> 
-            <h1 style ={{color:'tomato'}}>Nayok -- {props.name} </h1>
+            <h1 style ={{color:'tomato'}}>Nayok -- {props.name}  </h1>
             <p>He has done about 5 movies</p>
          </div>
 )}

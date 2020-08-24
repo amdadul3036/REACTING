a79c607610c659dc668899969f87240911e98ee6
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-24 07:26:46 -0700

    REACTING

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Nayok></Nayok>
+        <Nayok></Nayok>
+        <Nayok></Nayok>
+        <Nayok></Nayok>
+        <Nayok></Nayok>
+        <Nayok></Nayok>
+        <Nayok></Nayok>
       </header>
     </div>
   );
@@ -15,6 +21,9 @@ function App() {
 
 
 function Nayok(){
-  return <h1>KholNayok is Comming</h1>
+  return <div style = {{border:'2px magenta solid', margin:'3px', padding:'5px', borderRadius:'10%'}}> 
+            <h1 style ={{color:'tomato'}}>KholNayok is Comming</h1>
+            <p>I have done about 5 movies</p>
+         </div>
 }
 export default App;
